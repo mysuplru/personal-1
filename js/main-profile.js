e1b48173ecab27a628cc56ab48cbbe28e2b12caa
shushakova <shushakova111@yandex.ru>
2019-02-01 08:56:13 +0300

    доработки
    доработки + адаптив 960

---
+++ js/main-profile.js
@@ -0,0 +1,61 @@
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+
+    $('ul.list-unstyled li a').on('click', function(){
+        $('ul.list-unstyled a').removeClass('active-nav');
+        $(this).addClass('active-nav');
+    });
+
+
+
+    $(function () {
+        $('#profile-tabs li:first-child a').tab('show')
+    });
+
+
+    $(".input__block").on("click", function(){
+        $(this).find('.example').addClass("example-active");
+
+    });
+
+
+    $('.hide-btn').on('click', function(e){
+        e.preventDefault();
+
+        var
+            $this = $(this),
+            content = $(this).parents('.del-body').find('.del-product');
+
+        if(!$this.hasClass('show')){
+            $this.toggleClass('show');
+            content.slideDown();
+        }
+
+        $('.del-product_close').on('click', function(){
+            $this.removeClass('show');
+            content.slideUp();
+        });
+    });
+
+
+    $('#edit').on('click', function () {
+        $('.input-item').find("input").removeAttr("disabled");
+        $('#edit').hide();
+        $('#save').show();
+
+        });
+
+    $('#save').on('click', function () {
+        $('.input-item').find("input").attr('disabled', 'true');
+        $('#save').hide();
+        $('#edit').show();
+    })
+
+
+
+});
